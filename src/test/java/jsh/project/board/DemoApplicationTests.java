673a07b7131fdd8034da1422067578e4a0389291
장세훈 <jangsehun92@gmail.com>
2020-02-13 18:33:48 +0900

    add local project

---
+++ src/test/java/jsh/project/board/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.project.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
